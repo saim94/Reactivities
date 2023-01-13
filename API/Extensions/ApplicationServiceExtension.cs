9b2a2d556cb543b6ba1640cfad7edd80d8a9cfac
Saim Sajid <saim98@live.com>
2023-01-13 12:59:46 +0500

    End of section 4

---
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,38 @@
+﻿using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddEndpointsApiExplorer(); //Add swaggger Service
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            }
+            );
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                }
+                );
+            });
+
+            services.AddMediatR(typeof(List.Handler)); // needs to tell it where handler is located ie class/path. 
+                                                               //typeof(List.Handler) Tells this is type of handler or this is the location of handler.
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,7 @@
+using API.Extensions;
+using Application.Activities;
+using Application.Core;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -7,22 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add any services in this section
 builder.Services.AddControllers(); //Add Controllers Service
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer(); //Add swaggger Service
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-    {
-        opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-    }
-);
 
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    }
-    );
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build(); //Build the APP
 
